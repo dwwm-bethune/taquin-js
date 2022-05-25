074003ce8c13535d7004e67f48c42e087c7f9950
Matthieu Mota <matthieumota@gmail.com>
2022-05-25 18:51:38 +0200

    Fix condition

--- app.js
+++ app.js
@@ -19,16 +19,16 @@ $('.bloc').click(function () {
 
     console.log('diff: ' + diffX, diffY);
 
-    // On empêche le déplacement de plus de 2 blocs d'écarts
+    // On empêche le déplacement de plus de 2 blocs d'écarts (Logique 1)
     // Et on empêche la diagonale
     if (diffX > 100 || diffY > 100 || diffX >= 100 && diffY >= 100) {
-        return; // On arrête le code donc on ne bouge pas le bloc
+        // return; // On arrête le code donc on ne bouge pas le bloc
     }
 
-    // @todo Essayez de faire le if dans le sens inverse
-    // if (diffX == 100 || diffY == 100 && !(diffX == 100 && diffY == 100)) {
+    // On peut aussi s'assurer de faire le déplacement seulement s'il est correct (Logique 2)
+    if (diffX <= 100 && diffY == 0 || diffY <= 100 && diffX == 0) {
         // Inverser la position des blocs
         $(this).css({ top: emptyY, left: emptyX });
         $('.bloc-empty').css({ top: blocY, left: blocX });
-    // }
+    }
 });
